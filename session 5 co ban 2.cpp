cef244e2da7c58b734de8cf3ed378f085d1ffa63
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-25 23:23:19 +0700

    session 5 cơ bản 2

---
+++ session 5 co ban 2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	int n ;
+	int h =5 ;
+	while(1){
+	printf("moi ban nhap(n): ");
+	scanf("%d",&n);
+	if(n==5){
+		printf("so nhap trung voi so cho truoc ");
+		break;
+		}else{
+			printf("so nhap k trung voi so cho truoc\n");
+		
+	    }
+	}
+	return 0;
+}
